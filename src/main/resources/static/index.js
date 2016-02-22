b68cf476c5740d0fff73087770acf72d4fd35a04
Karolas <karolisb694@gmail.com>
2016-02-22 21:20:53 +0200

    try login + sql

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -504,7 +504,7 @@ var demoApp = angular.module('demoApp',['ngRoute', 'ngCookies']);
             $scope.code = null;
             $scope.response = null;
 
-            $http({method: 'GET', url: '/api/history', cache: $templateCache}).
+            $http({method: 'GET', url: '/api/history/gettable', cache: $templateCache}).
               then(function(response) {
                 $scope.status = response.status;
                 $scope.data = response.data;
