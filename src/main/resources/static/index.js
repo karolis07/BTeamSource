c005deabf04c2da5b157a849dddfa31b1dcfe696
markussohn <markus.justinas@gmail.com>
2016-02-23 00:01:01 +0200

    simple hardcoded login

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -6,15 +6,13 @@ DEL USER ID SAUGOJIMO IR PERDAVIMO NAUDOTI $ROOTSCOPE.USERID
 
 document.cookie = "";
 
-var authentication = angular.module('Authentication', []);
-var home = angular.module('Home', []);
-var demoApp = angular.module('demoApp',['ngRoute', 'ngCookies', 'Authentication', 'Home']);
+var demoApp = angular.module('demoApp',['ngRoute', 'ngCookies']);
 
     demoApp.config(function($routeProvider){
         $routeProvider
             .when('/',
             {
-                controller: 'LoginController',
+                controller: 'loginController',
                 templateUrl: 'pages/login.html'
             })
             .when('/register',
@@ -57,6 +55,17 @@ var demoApp = angular.module('demoApp',['ngRoute', 'ngCookies', 'Authentication'
             $scope.cookies = document.cookie;
         }]);
 
+        demoApp.controller('loginController', ['$scope', '$location', function($scope, $location){
+            $scope.submit = function() {
+                var email = $scope.email;
+                var password = $scope.password;
+                if($scope.email == 'admin@admin.lt' && $scope.password == 'admin'){
+                    $location.path('/home');
+                }
+            }
+
+        }]);
+
 
         demoApp.controller('languages',['$scope', '$http', '$rootScope', '$templateCache',function($scope, $http, $rootScope, $templateCache) {
 
@@ -82,235 +91,7 @@ var demoApp = angular.module('demoApp',['ngRoute', 'ngCookies', 'Authentication'
                 });
             };
         }]);
-//------------------------------------IF PROBLEMS WITH LOGIN, COMMENT CODE BELOW---------------------------
-        demoApp.run(['$rootScope', '$location', '$cookieStore', '$http',
 
-                    function ($rootScope, $location, $cookieStore, $http) {
-
-                        // keep user logged in after page refresh
-                        $rootScope.globals = $cookieStore.get('globals') || {};
-                        if ($rootScope.globals.currentUser) {
-                            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
-                        }
-                        $rootScope.$on('$locationChangeStart', function (event, next, current) {
-                            // redirect to login page if not logged in
-                            if ($location.path() !== '/' && !$rootScope.globals.currentUser) {
-                                $location.path('/');
-                            }
-                        });
-                    }]);
-
-        authentication.factory('AuthenticationService',
-                           ['Base64', '$http', '$cookieStore', '$rootScope', '$timeout',
-                           function (Base64, $http, $cookieStore, $rootScope, $timeout) {
-                                                      var service = {};
-
-                               service.Login = function (username, password, callback) {
-
-                               /* Dummy authentication for testing, uses $timeout to simulate api call
-
-                                            ----------------------------------------------*/
-
-                                           $timeout(function(){
-
-                                               var response = { success: username === 'admin@admin.lt' && password === 'admin' };
-
-                                               if(!response.success) {
-
-                                                   response.message = 'Username or password is incorrect';
-
-                                               }
-
-                                               callback(response);
-
-                                           }, 1000);
-
-                             /* Use this for real authentication
-
-                                    ----------------------------------------------*/
-
-//                                   $http.get('/api/authenticate/' + $scope.username + '/' + $scope.password)
-//                                       .success(function (response) {
-//                                           callback(response);
-//                                      });
-                               };
-                               service.SetCredentials = function (username, password) {
-                                   var authdata = Base64.encode(username + ':' + password);
-
-                                   $rootScope.globals = {
-                                       currentUser: {
-                                           username: username,
-
-                                           authdata: authdata
-                                       }
-                                   };
-
-                                   $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
-
-                                   $cookieStore.put('globals', $rootScope.globals);
-
-                               };
-
-                               service.ClearCredentials = function () {
-
-                                   $rootScope.globals = {};
-
-                                   $cookieStore.remove('globals');
-
-                                   $http.defaults.headers.common.Authorization = 'Basic ';
-
-                               };
-
-                               return service;
-
-                           }])
-                           .factory('Base64', function () {
-                               /* jshint ignore:start */
-
-                                  var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-
-                                  return {
-                                      encode: function (input) {
-                                          var output = "";
-                                          var chr1, chr2, chr3 = "";
-                                          var enc1, enc2, enc3, enc4 = "";
-                                          var i = 0;
-
-                                          do {
-
-                                              chr1 = input.charCodeAt(i++);
-                                              chr2 = input.charCodeAt(i++);
-                                              chr3 = input.charCodeAt(i++);
-
-                                              enc1 = chr1 >> 2;
-                                              enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-                                              enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-                                              enc4 = chr3 & 63;
-
-                                              if (isNaN(chr2)) {
-
-                                                  enc3 = enc4 = 64;
-
-                                              } else if (isNaN(chr3)) {
-
-                                                  enc4 = 64;
-
-                                              }
-
-                                              output = output +
-                                                  keyStr.charAt(enc1) +
-                                                  keyStr.charAt(enc2) +
-                                                  keyStr.charAt(enc3) +
-                                                  keyStr.charAt(enc4);
-
-                                              chr1 = chr2 = chr3 = "";
-                                              enc1 = enc2 = enc3 = enc4 = "";
-                                          } while (i < input.length);
-
-                                          return output;
-                                      },
-
-                                      decode: function (input) {
-
-                                          var output = "";
-                                          var chr1, chr2, chr3 = "";
-                                          var enc1, enc2, enc3, enc4 = "";
-                                          var i = 0;
-
-                                          // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-
-                                          var base64test = /[^A-Za-z0-9\+\/\=]/g;
-                                          if (base64test.exec(input)) {
-                                              window.alert("There were invalid base64 characters in the input text.\n" +
-
-                                                  "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-
-                                                  "Expect errors in decoding.");
-
-                                          }
-                                          input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-                                          do {
-
-                                              enc1 = keyStr.indexOf(input.charAt(i++));
-                                              enc2 = keyStr.indexOf(input.charAt(i++));
-                                              enc3 = keyStr.indexOf(input.charAt(i++));
-                                              enc4 = keyStr.indexOf(input.charAt(i++));
-
-
-                                              chr1 = (enc1 << 2) | (enc2 >> 4);
-                                              chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-                                              chr3 = ((enc3 & 3) << 6) | enc4;
-
-
-                                              output = output + String.fromCharCode(chr1);
-
-
-                                              if (enc3 != 64) {
-
-                                                  output = output + String.fromCharCode(chr2);
-
-                                              }
-
-                                              if (enc4 != 64) {
-
-                                                  output = output + String.fromCharCode(chr3);
-
-                                              }
-
-                                              chr1 = chr2 = chr3 = "";
-                                              enc1 = enc2 = enc3 = enc4 = "";
-
-                                          } while (i < input.length);
-
-                                          return output;
-
-                                      }
-
-                                  };
-
-                                  /* jshint ignore:end */
-
-                              });
-
-        authentication.controller('LoginController',
-
-                           ['$scope', '$rootScope', '$location', 'AuthenticationService',
-
-                           function ($scope, $rootScope, $location, AuthenticationService) {
-
-                               // reset login status
-
-                               AuthenticationService.ClearCredentials();
-
-
-
-                               $scope.login = function () {
-
-                                   $scope.dataLoading = true;
-
-                                   AuthenticationService.Login($scope.username, $scope.password, function(response) {
-
-                                       if(response.success) {
-
-                                           AuthenticationService.SetCredentials($scope.username, $scope.password);
-
-                                           $location.path('/home');
-
-                                       } else {
-
-                                           $scope.error = response.message;
-
-                                           $scope.dataLoading = false;
-
-                                       }
-
-                                   });
-
-                               };
-
-                           }]);
-////////////--------------------------------IF PROBLEMS WITH LOGIN, COMMENT THE CODE ABOVE-----------------------
 
 
         demoApp.directive('datepicker', function() {
